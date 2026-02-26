653e0bd09613a63db0b3691c1021f777817788d5
RuttiTuttii <61704909+RuttiTuttii@users.noreply.github.com>
2026-02-26 14:21:10 +0300

    программа, показывающая, как работает куча и сборщик мусора

---
+++ Lections/Lection/Lection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+Person person = new Person() { name = "Roman", age = 23};
+void ChangePerson(ref Person person)
+{
+    person.name = "NoName";
+    person = new Person() { name = "Bill", age = 123 };
+    Console.WriteLine(person.name);
+}
+
+ChangePerson(ref person);
+Console.WriteLine(person.name);
+Console.WriteLine(person.age);
+
+
+
+class Person
+{
+    public string name = "";
+    public int age;
+}
+
